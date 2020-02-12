2e54568a7140f4d22baa967f166efea96c29f0df
philippeschwab.fr <ps77240@hotmail.fr>
2020-02-12 11:31:02 +0100

    modification class reservation et group

--- src/main/java/com/simplon/voyageaplusieurs/Model/Group.java
+++ src/main/java/com/simplon/voyageaplusieurs/Model/Group.java
@@ -1,6 +1,27 @@
 package com.simplon.voyageaplusieurs.Model;
 
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Group {
+    @Id
+    @SequenceGenerator(name = "group_seq_id", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "group_seq_id")
     private Long id;
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    @OneToMany(mappedBy = "group")
+    private List<Reservation> reservations = new ArrayList<>();
+
 
 }
--- src/main/java/com/simplon/voyageaplusieurs/Model/Reservation.java
+++ src/main/java/com/simplon/voyageaplusieurs/Model/Reservation.java
@@ -1,15 +1,69 @@
 package com.simplon.voyageaplusieurs.Model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
 import java.util.Date;
 
+
+@Entity
 public class Reservation {
+
+    @Id
+    @SequenceGenerator(name = "reservation_seq_id", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reservation_seq_id")
     private Long id;
+
+    @Column(nullable = false)
     private Trip trip;
+
+     @ManyToOne
+
     private Group group;
+
+    @Column(nullable = false)
     private String bedRoomNumber;
+
+    @Column(nullable = false)
     private Double globalPrice;
+
+    @Column(nullable = false)
     private Date date;
+
+    @Column()
     private PensionType pensionType;
 
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+
+    public String getBedRoomNumber() {
+        return bedRoomNumber;
+    }
+
+
+    public double getGlobalPrice() {
+        return globalPrice;
+
+    }
+
+
+    public Date getDate() {
+        return date;
+
+    }
 
 }
