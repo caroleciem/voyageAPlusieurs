224d7c57073e0214615ab3738cbf73a204040dec
ciemny <carole.ciemny.devweb@gmail.com>
2020-02-12 09:48:02 +0100

    classe complete sans many

---
+++ src/main/java/com/simplon/voyageaplusieurs/Model/Role.java
@@ -0,0 +1,6 @@
+package com.simplon.voyageaplusieurs.Model;
+
+public class Role {
+    private Long id;
+    private RoleType roleType;
+}
---
+++ src/main/java/com/simplon/voyageaplusieurs/Model/RoleType.java
@@ -0,0 +1,4 @@
+package com.simplon.voyageaplusieurs.Model;
+
+public enum RoleType { PAYEUR, ORGANIZER, PARTICIPANT
+}
