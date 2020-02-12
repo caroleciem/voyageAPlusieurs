8136a17024ad502cc63cecf19acfb14c2ca83597
ciemny <carole.ciemny.devweb@gmail.com>
2020-02-12 09:54:13 +0100

    modif trip

--- src/main/java/com/simplon/voyageaplusieurs/Model/Trip.java
+++ src/main/java/com/simplon/voyageaplusieurs/Model/Trip.java
@@ -1,10 +1,14 @@
 package com.simplon.voyageaplusieurs.Model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Trip {
     private Long id;
     private Double pricePerPers;
     private Integer duration;
     private String descriptiveRestauration;
     private Destination destination;
+    private PensionType pensionType;
 
 }
