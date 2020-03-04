7edd06402ac4f6724bc6141682d075908a9a531b
Pierre <pierre.jolivet.devweb@gmail.com>
2020-03-04 16:37:46 +0100

    Add reservation,person,payment data.sql insertion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,18 @@ insert into trip (id, descriptive_restauration, duration, price_per_pers, destin
 
 insert into destination(id,country,continent,descriptive, situation) values (3,'Italie', 'Europe' ,'Profitez du carnaval','près du lac');
 insert into trip (id, descriptive_restauration, duration, price_per_pers, destination_id) values (4, 'bas les masques', 15, 428, 3);
+
+insert into reservation (id, BED_ROOM_NUMBER, GLOBAL_PRICE, date, PENSION_TYPE) values (1,'2', 1000 ,'2020-04-02',1);
+insert into reservation (id, BED_ROOM_NUMBER, GLOBAL_PRICE, date, PENSION_TYPE) values (2,'4', 5000 ,'2020-04-02',1);
+
+insert into reservation (id, BED_ROOM_NUMBER, GLOBAL_PRICE, date, PENSION_TYPE) values (3,'1', 1000 ,'2020-04-02',2);
+insert into reservation (id, BED_ROOM_NUMBER, GLOBAL_PRICE, date, PENSION_TYPE) values (4,'2', 2000 ,'2020-04-02',2);
+
+insert into reservation (id, BED_ROOM_NUMBER, GLOBAL_PRICE, date, PENSION_TYPE) values (5,'4', 4000 ,'2020-04-02',3);
+insert into reservation (id, BED_ROOM_NUMBER, GLOBAL_PRICE, date, PENSION_TYPE) values (6,'8', 8000 ,'2020-04-02',3);
+
+insert into person(id,address,city,civility,country,email,first_name,name,phone,zip_code) values (1,'10, rue des restaurants','Montreuil','Monsieur','France','prenom.nom@test.com','test-prenom','test-nom','06-12-34-56-67','93100');
+insert into person(id,address,city,civility,country,email,first_name,name,phone,zip_code) values (2,'10, rue des bars','Montreuil','Madame','France','prenom2.nom2@test.com','test-prenom2','test-nom2','06-09-87-65-43','93100');
+insert into person(id,address,city,civility,country,email,first_name,name,phone,zip_code) values (3,'10, rue des hotels','Montreuil','Monsieur','France','prenom3.nom3@test.com','test-prenom3','test-nom3','06-09-87-65-43','93100');
+
+insert into payment(id,amount,person_id,reservation_id) values(1,100,2,1);
